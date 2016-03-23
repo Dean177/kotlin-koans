0e574971f9e0399edc0e6e9d7177686cbfc11c96
Dean Merchant <dean.merchant@softwire.com>
2016-03-23 15:59:58 +0000

    5 - 12

--- src/i_introduction/_10_Object_Expressions/ObjectExpressions.kt
+++ src/i_introduction/_10_Object_Expressions/ObjectExpressions.kt
@@ -18,6 +18,12 @@ fun todoTask10(): Nothing = TODO(
 
 fun task10(): List<Int> {
     val arrayList = arrayListOf(1, 5, 2)
-    Collections.sort(arrayList, todoTask10())
+//    Collections.sort(arrayList, { left, right -> right - left })
+    Collections.sort(arrayList, object : Comparator<Int> {
+        override fun compare(left: Int, right: Int):  Int {
+            return right - left
+        }
+    })
+
     return arrayList
 }
